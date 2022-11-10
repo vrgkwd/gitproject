457928a98990d9db5c63af0fc1d4e711c6ad58e3
dev01 <dev01@gmai.com>
2022-11-10 13:50:16 +0530

    line added by dev02

--- GitDemoProject/src/com/app/TestDevlopers.java
+++ GitDemoProject/src/com/app/TestDevlopers.java
@@ -5,6 +5,8 @@ public class TestDevlopers {
 	public static void main(String[] args) {
 
 		System.out.println("Testing for developer dev 01");
+		
+		System.out.println("Testing for developer dev 02");
 	}
 
 }

092c03d474e73f241fb95eef10e140563d8dfeb2
dev01 <dev01@gmai.com>
2022-11-11 11:20:18 +0530

    commit by Dev01 Branch02

--- GitDemoProject/src/com/app/vtz/VertechzClass.java
+++ GitDemoProject/src/com/app/vtz/VertechzClass.java
@@ -1,8 +1,9 @@
 package com.app.vtz;
 
 public class VertechzClass {
-public static void main(String[] args) {
-	System.out.println("On call selection-dev03");
-	System.out.println("On call selection-dev02");
-}
+	public static void main(String[] args) {
+		System.out.println("On call selection-dev03");
+		System.out.println("On call selection-dev02");
+		System.out.println("On call selection-dev01");
+	}
 }
